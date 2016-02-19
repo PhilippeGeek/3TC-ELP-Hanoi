c83180f9a823adba423efd1e31e5cef17e7f65b8
Philippe Vienne <philippe@vienne.me>
2016-02-19 14:29:15 +0100

    Add unit testing

---
+++ src/test/java/tc/elp/java/datastructures/Test.java
@@ -0,0 +1,12 @@
+package tc.elp.java.datastructures;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+    @org.junit.Test
+    public void trueTest(){
+        assertEquals(1,1);
+    }
+
+}
